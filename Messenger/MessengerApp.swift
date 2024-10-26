d1247a8a03cd57382df1dbcf3fd1871271d9da8c
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-10-26 14:17:34 +0400

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Hakob Ghlijyan on 26.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
