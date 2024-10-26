b3b472e26888fac12739568d7d393ace52fa245e
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-10-26 14:34:27 +0400

    End Module 01 Start Module 02

---
+++ Messenger/Core/Authentication/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Messenger
+//
+//  Created by Hakob Ghlijyan on 26.10.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LoginView()
+}
