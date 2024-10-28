61915cddd550d27a263837c6d39275111401b9bd
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-10-28 18:26:17 +0400

    End Module 05 Add Row inbox Add Active scroll view Add two InboxView

---
+++ Messenger/Core/Inbox/InboxRowView.swift
@@ -0,0 +1,43 @@
+//
+//  InboxRowView.swift
+//  Messenger
+//
+//  Created by Hakob Ghlijyan on 28.10.2024.
+//
+
+import SwiftUI
+
+struct InboxRowView: View {
+    var body: some View {
+        HStack(alignment: .top, spacing: 12) {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 64, height: 64)
+                .foregroundStyle(Color(.systemGray4))
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Hakob Ghlijyan")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                
+                Text("Some test message for preview, flabbergasted by the simplicity of SwiftUI")
+                    .font(.subheadline)
+                    .foregroundStyle(.gray)
+                    .lineLimit(2)
+                    .frame(maxWidth: UIScreen.main.bounds.width - 100, alignment: .leading) // for first line is not in time section
+            }
+            
+            HStack {
+                Text("12:00 PM")
+                Image(systemName: "chevron.right")
+            }
+            .font(.footnote)
+            .foregroundStyle(.gray)
+        }
+        .frame(height: 72)
+    }
+}
+
+#Preview {
+    InboxRowView()
+}
---
+++ Messenger/Core/Inbox/InboxView.swift
@@ -0,0 +1,53 @@
+//
+//  InboxView.swift
+//  Messenger
+//
+//  Created by Hakob Ghlijyan on 28.10.2024.
+//
+
+import SwiftUI
+
+struct InboxView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                ActiveNowView()
+                
+                List {
+                    ForEach(0...10, id: \.self) { message in
+                        InboxRowView()
+                    }
+                }
+                .listStyle(.plain)
+                .frame(height: UIScreen.main.bounds.height - 120)
+      
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    HStack {
+                        Image(systemName: "person.circle.fill")
+                        
+                        Text("Charts")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "square.and.pencil.circle.fill")
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                            .foregroundStyle(.black, Color(.systemGray5))
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    InboxView()
+}
