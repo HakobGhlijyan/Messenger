bd41679f1eda02d822c43bdc1376029d62f236bc
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-10-28 19:13:52 +0400

    Start Module 06 module 06 Profile View Settings Add view is module 04 dont add in text dexcription.. i ts real module 06

---
+++ Messenger/Core/Profile/VIewModel/SettingsOptionsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsOptionsViewModel.swift
+//  Messenger
+//
+//  Created by Hakob Ghlijyan on 28.10.2024.
+//
+
+import SwiftUI
+
+final class SettingsOptionsViewModel: ObservableObject {
+// 4.26 min
+}
---
+++ Messenger/Core/Profile/View/ProfileView.swift
@@ -0,0 +1,76 @@
+//
+//  ProfileView.swift
+//  Messenger
+//
+//  Created by Hakob Ghlijyan on 28.10.2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            //Header
+            VStack {
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .foregroundStyle(Color(.systemGray4))
+                
+                Text("Hakob Ghlijyan")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+            }
+            //List
+            List {
+                Section {
+                    ForEach(0...5, id: \.self) { option in
+                        HStack {
+                            Image(systemName: "bell.circle.fill")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundStyle(Color(.systemGray))
+                            
+                            Text("Notification")
+                                .font(.subheadline)
+                        }
+                    }
+                }
+                
+                Section {
+                    Button("Log Out") {
+                        
+                    }
+                    Button("Delete Account") {
+                        
+                    }
+                }
+                .foregroundStyle(.red)
+                
+                
+            }
+            
+            
+        }
+        
+//        LazyVStack(spacing: 0, pinnedViews: [.sectionHeaders]) {
+//            Section(header: Text("foo") ) {
+//                LazyVStack(spacing: 0, pinnedViews: [.sectionHeaders]) {
+//                    Section (header: Text("bar") ) {
+//                        Text("1")
+//                        Text("2")
+//                    }
+//                    Section (header: Text("baz") ) {
+//                        Text("1")
+//                        Text("2")
+//                    }
+//                }
+//            }
+//        }
+        
+    }
+}
+
+#Preview {
+    ProfileView()
+}
